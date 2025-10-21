5ac55b2df951c5499ebc82219d674a27b6b6f5b9
MudMoh <mudesir@gmail.com>
2025-10-21 15:23:01 +0300

    Wrap the Application with ErrorBoundary

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,12 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client/react";
-import client from "@/graphql/apolloClient";
+// import { ApolloProvider } from "@apollo/client/react";
+// import client from "@/graphql/apolloClient";
+import ErrorBoundary from '@/components/ErrorBoundary';
 
-export default function App({ Component, pageProps }: AppProps) {
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <ApolloProvider client={client}>
+//       <Component {...pageProps} />
+//     </ApolloProvider>
+//   )
+// }
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <ErrorBoundary>
       <Component {...pageProps} />
-    </ApolloProvider>
-  )
+    </ErrorBoundary>
+  );
 }
+
+export default MyApp;
